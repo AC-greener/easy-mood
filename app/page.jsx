ea66dd071fa940860ecc426a9a20746ad6417376
zhutongtong <15929774304@163.com>
2024-07-27 15:16:15 +0800

    title

--- app/page.jsx
+++ app/page.jsx
@@ -75,7 +75,7 @@ export default function Home() {
   return (
     <main className="min-h-screen  bg-accent  p-8 pl-64 pr-64">
       <div className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold text-center mb-8">Easy Moods</h1>
+        <h1 className="text-3xl font-bold text-center mb-8">Easy Mood</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {Object.entries(sounds).map(([soundKey, { icon, label }]) => (
             <SoundCard
